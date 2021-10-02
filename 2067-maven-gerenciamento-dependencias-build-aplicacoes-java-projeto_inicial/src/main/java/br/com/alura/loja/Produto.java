6e3adf5cf8c9791f46f48e8e3a2dd3c66bc4b318
Lucas Santos <lucas@aluno.faculdadeimpacta.com.br>
2021-10-02 13:56:49 -0300

    envio do projeto de exemplo Maven desenvolvido durante o curso

--- 2067-maven-gerenciamento-dependencias-build-aplicacoes-java-projeto_inicial/src/main/java/br/com/alura/loja/Produto.java
+++ 2067-maven-gerenciamento-dependencias-build-aplicacoes-java-projeto_inicial/src/main/java/br/com/alura/loja/Produto.java
@@ -1,5 +1,26 @@
 package br.com.alura.loja;
 
+import java.math.BigDecimal;
+
 public class Produto {
+
+    private String nomeProd;
+    private BigDecimal valorProd;
+
+    public Produto(String nomeProd, BigDecimal valorProd) {
+        this.nomeProd = nomeProd;
+        this.valorProd = valorProd;
+    }
+
+    public String getNomeProd() {
+        return nomeProd;
+    }
+
+
+
+    public BigDecimal getValorProd() {
+        return valorProd;
+    }
+
 
 }
--- 2067-maven-gerenciamento-dependencias-build-aplicacoes-java-projeto_inicial/src/test/java/br/com/alura/loja/ProdutoTest.java
+++ 2067-maven-gerenciamento-dependencias-build-aplicacoes-java-projeto_inicial/src/test/java/br/com/alura/loja/ProdutoTest.java
@@ -4,11 +4,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 public class ProdutoTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+
+		Produto p = new Produto("teste", BigDecimal.TEN);
+		assertEquals("teste",p.getNomeProd());
+		assertEquals(BigDecimal.TEN,p.getValorProd());
 	}
 
 }
